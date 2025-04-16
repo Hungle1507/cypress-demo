09d712666cc26ae2a9b36d74a36a9948d5823860
hunglm <leminhhung1507@gmail.com>
2025-04-16 23:07:07 +0700

    add comment testscript

--- cypress/e2e/loginPage/login.cy.js
+++ cypress/e2e/loginPage/login.cy.js
@@ -1,7 +1,7 @@
 import LoginPage from './login'
 describe('Admin Portal - Login (Positive)', () => {
-  LoginPage.TC_01();
-  LoginPage.TC_02(); 
-  LoginPage.TC_03();
-  LoginPage.TC_04();
+  LoginPage.TC_01(); //Successful Login with Valid Credentials
+  LoginPage.TC_02(); //Login with Invalid Email Format
+  LoginPage.TC_03();//Login with Valid Email but Incorrect Password
+  LoginPage.TC_04();//Login with Empty Email Field
   })
